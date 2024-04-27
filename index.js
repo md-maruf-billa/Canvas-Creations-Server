28349faf752ca050f55a1b8a2d11f8c0da6e4b20
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-04-27 11:35:24 +0600

    Added mongo server and hide api key using .env and add middel ware

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 7000;
+const cors = require('cors');
+
+
+
+// --------Middle ware hare---------
+app.use(cors());
+app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.fp7vkua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fp7vkua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +30,9 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get("/",(req,res)=>{
+        res.send("Hehe mama")
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -31,8 +42,6 @@ run().catch(console.dir);
 
 
 
-// canvas-creations
-// PtVC2TSuhm6NLSIF
 
 // -------------listening port----------
 
