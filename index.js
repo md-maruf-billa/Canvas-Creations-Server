f35d96f8e6b6c82966a692a2214ccfe793544467
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-04-28 03:21:39 +0600

    fixed function issue

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
 const express = require('express');
+const cors = require('cors')
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 7000;
-const cors = require('cors');
 
 
 
 // --------Middle ware hare---------
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173"]
+}));
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fp7vkua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    app.get("/",(req,res)=>{
-        res.send("Hehe mama")
-    })
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+
+        //------------create database and collection------------------
+        const craftCollection = client.db("canvas-creation").collection("allArtAndCraft");
+
+        //------------GET DATA FORM DATABASE----------
+
+        app.get("/", async (req, res) => {
+            const allData = await craftCollection.find().toArray();
+            res.send(allData)
+        })
+
+        // ----------get specific data from database-------
+        app.get("/details/:id",async(req,res)=>{
+            const id = req.params;
+            const quire = {_id: new ObjectId(id)}
+            const result = await craftCollection.findOne(quire);
+            res.send(result);
+            
+            
+        })
+        //-------------GET DATA USING EMAIL----------
+
+        // ------------POST REQUESTS HARE---------
+        app.post("/add-craft-items", async (req, res) => {
+            const data = req.body;
+            const result = await craftCollection.insertOne(data);
+            res.send(result)
+        })
+
+        
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -45,6 +70,6 @@ run().catch(console.dir);
 
 // -------------listening port----------
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`The server is running ${port}`)
 })
