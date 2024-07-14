a35ed4071ae62023934522df62b75c2f5597f330
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-07-14 20:33:53 +0600

    converted privet repo to public repo

---
+++ customMiddleWare.js
@@ -0,0 +1,5 @@
+const verifyUserPermission = (req,res,next) =>{
+    console.log("valo aceo to tumi?")
+}
+
+export {verifyUserPermission}
--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const express = require('express');
-const cors = require('cors')
-require('dotenv').config()
+import express, { json } from 'express';
+import cors from 'cors';
+import jwt from 'jsonwebtoken'
+import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
+import 'dotenv/config'
+import cookieParser from 'cookie-parser';
+// import { verifyUserPermission } from './customMiddleWare.js';
+
+
+
 const app = express();
 const port = process.env.PORT || 7000;
 
 
 
 // --------Middle ware hare---------
-app.use(cors());
-app.use(express.json())
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true,
+}));
+app.use(json())
+app.use(cookieParser())
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const verifyUserPermission = async (req, res, next) => {
+    const cookie = req.cookies.access_token;
+    if (!cookie) {
+        return res.status(401).send({ message: "unauthorize" })
+    }
+    jwt.verify(cookie, process.env.COOKIE_SECRETE_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "unauthorize" })
+        }
+        req.user = decoded;
+        next()
+    })
+
+}
+
+
+
+
+
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fp7vkua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,7 +62,7 @@ async function run() {
         const allCategories = client.db("canvas-creation").collection("allCategories");
 
 
-        app.get("/categories", async(req,res)=>{
+        app.get("/categories", async (req, res) => {
             const result = await allCategories.find().toArray();
             res.send(result);
         })
@@ -44,7 +75,7 @@ async function run() {
         })
 
         // ----------get specific data from database-------
-        app.get("/details/:id", async (req, res) => {
+        app.get("/details/:id", verifyUserPermission, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await craftCollection.findOne(query);
@@ -53,20 +84,28 @@ async function run() {
 
         })
         //-------------GET DATA USING EMAIL----------
-        app.get("/my-art-and-craftList/:email", async (req, res) => {
+        app.get("/my-art-and-craftList/:email", verifyUserPermission, async (req, res) => {
+            const user = req.user;
             const email = req.params;
-            const query = { email: email.email };
 
-            //-----------Get from Database-------
-            const data = craftCollection.find(query);
-            const result = await data.toArray()
-            res.send(result)
+            //---------verify email---------
+            if (user.email == email.email) {
+                const query = { email: email.email };
+
+                //-----------Get from Database-------
+                const data = craftCollection.find(query);
+                const result = await data.toArray()
+                return res.send(result)
+            }
+            res.status(401).send({ message: "unauthorize" })
+
+
         })
 
         //----------GET DATA USING CATEGORIES NAME-------------
-        app.get("/category/:categoryName", async(req,res)=>{
+        app.get("/category/:categoryName", async (req, res) => {
             const category = req.params.categoryName;
-            const query = {category:category}
+            const query = { category: category }
             const result = await craftCollection.find(query).toArray();
             res.send(result)
         })
@@ -76,6 +115,31 @@ async function run() {
             const data = req.body;
             const result = await craftCollection.insertOne(data);
             res.send(result)
+        })
+
+        //-----------GET USER REQUEST AND SEND A TOKEN-------------
+        app.post("/verify", async (req, res) => {
+            const userData = req.body;
+            const token = jwt.sign(userData, process.env.COOKIE_SECRETE_KEY, { expiresIn: '1h' })
+
+
+            res
+                .cookie("access_token", token, {
+                    httpOnly: true,
+                    secure: false,
+                })
+                .status(200)
+                .send({ message: "success" })
+        })
+
+
+        // ------------Log Out user-------------
+        app.get("/logout", async(req,res)=>{
+            
+            console.log("hello")
+            res
+            .clearCookie("access_token",{maxAge:0})
+            .send({message:"Logout successful"})
         })
 
 
@@ -97,7 +161,7 @@ async function run() {
                     photoURL: data.photoURL
                 }
             }
-            const result = await craftCollection.updateOne(query,updateItem);
+            const result = await craftCollection.updateOne(query, updateItem);
             res.send(result);
         })
 
